ad39746bf5badc41ee8ab627db923d7ec1e17b6f
TimZoe <contact@trophie.de>
2021-12-14 13:58:21 +0100

    BE

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "cvc10": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
